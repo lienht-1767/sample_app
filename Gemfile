4458d65e03cbe5859d17c5a08d72fd56eb48fbec
lienht-1767 <hoang.thi.lien@framgia.com>
2019-03-14 08:22:52 +0700

    chap-11 - account activate

--- Gemfile
+++ Gemfile
@@ -9,11 +9,9 @@ gem "puma", "~> 3.11"
 gem "sass-rails", "~> 5.0"
 gem "uglifier", ">= 1.3.0"
 gem "byebug"
-
 gem "coffee-rails", "~> 4.2"
 gem "turbolinks", "~> 5"
 gem "jbuilder", "~> 2.5"
-
 gem "bootsnap", ">= 1.1.0", require: false
 gem "i18n"
 gem "i18n-js"
@@ -26,6 +24,7 @@ gem "jquery-rails", "~> 4.3", ">= 4.3.1"
 gem "ffaker", "~> 2.2"
 gem "kaminari"
 gem "bootstrap-kaminari-views"
+gem "figaro"
 
 group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,16 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
 
     if user&.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == Settings.remember_me ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == Settings.remember_me ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message  = t ".inactivated"
+        message += t ".check_active"
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = t ".invalid"
       render :new
